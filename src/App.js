79202da2afbfaaa90a39c42793d94c9e4ac13df3
Ona <onushkis@gmail.com>
2021-12-30 17:38:58 +0100

    Add changes DONE

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import './App.css';
+import styled, {ThemeProvider} from 'styled-components';
+import {lightTheme, darkTheme, GlobalStyles} from './Themes.js'
+
+const StyledApp = styled.div `
+color: ${props => props.theme.fontColor};
+`;
+function App() {
+  const [theme, setTheme] = useState('light');
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  };
+
+  return (
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}> 
+    <GlobalStyles />
+        <StyledApp>
+          Hello World
+          <button onClick={() => themeToggler ()}> Change Themes</button>
+          </StyledApp>
+   </ThemeProvider>
+  )};
+export default App;
